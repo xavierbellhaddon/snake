023402b6a450cf59317caa6ea6420857f9775dc0
Austin Bell <snarkeet@gmail.com>
2020-10-29 23:18:13 -0400

    draw/update snake

--- game.js
+++ game.js
@@ -1,12 +1,24 @@
+import { update as updateSnake, draw as drawSnake, SNAKE_SPEED } from "./snake.js";
+
 let lastRenderTime = 0;
-const SNAKE_SPEED = 2;
 
 function main(currentTime) {
     window.requestAnimationFrame(main);
     const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
     if (secondsSinceLastRender < 1 / SNAKE_SPEED) return;
+
     lastRenderTime = currentTime;
-    console.log("Render")
+
+    update();
+    draw();
 }
 
 window.requestAnimationFrame(main);
+
+function update() {
+    updateSnake();
+};
+
+function draw() {
+    drawSnake();
+};
