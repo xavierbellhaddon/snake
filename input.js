35e4bd12bfce53874c2efaafa201e272e3453876
Austin Bell <snarkeet@gmail.com>
2020-10-30 01:07:28 -0400

    lastInputDirection

--- input.js
+++ input.js
@@ -1,22 +1,28 @@
 let inputDirection = { x: 0, y: 0 };
+let lastInputDirection = { x: 0, y: 0 };
 
 window.addEventListener("keydown", (e) => {
   switch (e.key) {
     case "ArrowUp":
-      inputDirection = { x: 0, y: -1 }
+      if (lastInputDirection.y !== 0) break;
+      inputDirection = { x: 0, y: -1 };
       break;
     case "ArrowDown":
-      inputDirection = { x: 0, y: 1 }
+        if (lastInputDirection.y !== 0) break;
+      inputDirection = { x: 0, y: 1 };
       break;
     case "ArrowLeft":
-      inputDirection = { x: -1, y: 0 }
+        if (lastInputDirection.x !== 0) break;
+      inputDirection = { x: -1, y: 0 };
       break;
     case "ArrowRight":
-      inputDirection = { x: 1, y: 0}
+        if (lastInputDirection.x !== 0) break;
+      inputDirection = { x: 1, y: 0};
       break;
   }
 });
 
 export function getInputDirection() {
+    lastInputDirection = inputDirection;
   return inputDirection;
 }
